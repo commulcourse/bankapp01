8c3db131cea683386b5ff90808fdebf1676bb5a9
commulcourse <commulcourse@gmail.com:git config --golbal user.email commulcourse@gmail.comgit config --global user.email commulcourse@gmail.com>
2023-02-16 20:46:58 +0900

    bankapp 프로젝트생성 및 세팅완료

---
+++ src/test/java/shop/mtcoding/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
